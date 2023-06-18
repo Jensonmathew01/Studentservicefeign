2950a187a8af0e70471943261f69c233c079aa84
Jensonmathew01 <Jenson.Mathew@ust.com>
2023-06-18 20:52:16 +0530

    18-06-2023

---
+++ src/test/java/com/ust/StudentServicesfiegen/StudentServicesfiegenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.StudentServicesfiegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServicesfiegenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
